625e91c2a0a9d74c050de521c80d5fb7df7ce3f0
Ilya <bugatti141@gmail.com>
2024-01-25 15:20:38 +0300

    README.MD

--- api/main.py
+++ api/main.py
@@ -6,4 +6,6 @@ from ngrok import ngrok
 from app import app
 
 if __name__ == "__main__":
+    listener = ngrok.forward(8000, authtoken="paste_your_ngrok_token_here")
+    print(f"Ingress established at {listener.url()}")
     uvicorn.run("app:app", reload=True, log_level="debug")
